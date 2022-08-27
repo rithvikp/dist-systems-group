adc34520b60fb00f0aed6d0823d2b96bf8560379
Rithvik <rtpanch@gmail.com>
2022-08-26 19:23:47 -0700

    Copy the basic portions of the script to begin updating them

---
+++ Scripts/orchestrator/experiment.py
@@ -0,0 +1,11 @@
+import os
+import os.path
+import sys
+import datetime
+import time
+import random
+import multiprocessing
+
+from util.ssh_util import *
+from util.ec2_util import *
+from util.prop_util import *
